e24a6fe7d6168a2edb841652e95de84055d10445
goofyww <akindofattitude@163.com>
2020-09-06 17:58:47 +0800

    开发环境搭建

---
+++ hadoop-train-v2/src/test/java/com/zcoox/hadoop/hdfs/HDFSApp.java
@@ -0,0 +1,21 @@
+package com.zcoox.hadoop.hdfs;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+
+import java.net.URI;
+
+/**
+ * HDFS App
+ */
+public class HDFSApp {
+
+    public static void main(String[] args) throws Exception {
+        Configuration config = new Configuration();
+        FileSystem fileSystem = FileSystem.get(new URI("hdfs://hadoop000:8020"), config, "hadoop");
+        boolean result = fileSystem.mkdirs(new Path("/hdfsapi/test"));
+        System.out.println(result);
+    }
+    
+}
